b9f1e82839660c7f1f8bf7feec52a940ef8ab78d
Joshua <joshua.greenslade@mail.utoronto.ca>
2017-03-02 12:50:45 -0500

    updated bug_test_6-broken to include description

--- d3_bug_tests/bug_test_6-broken.py
+++ d3_bug_tests/bug_test_6-broken.py
@@ -1,4 +1,8 @@
 #animate does not run but it should
+#the animation.FuncAnimation(fig, animate) is not saved in a var so python's garbage collection cleans it up
+#before it can execute
+
+#fix by possibly saving the animation in figure
 
 import matplotlib
 from matplotlib import pyplot as plt
