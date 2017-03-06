c0bb16a1d6073d12953a9e90cae61f8c470c14cd
VethushaanAkilathasan <vethush@gmail.com>
2017-03-05 22:56:59 -0500

    repo cleanup

--- d3_bug_tests/bug_test_6.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-from matplotlib import pyplot as plt
-fig = plt.figure()
-def animate(i):
-    print("hi")
-from matplotlib import animation
-anim=animation.FuncAnimation(fig, animate)
-plt.show()
